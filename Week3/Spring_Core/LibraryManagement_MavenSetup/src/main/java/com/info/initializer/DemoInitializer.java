1a3957bc85f006134f493ebfdc69f645e86be6ad
siddumr <mrsiddu2112@gmail.com>
2025-07-05 11:07:30 +0530

    weekex3

---
+++ Week3/Spring_Core/LibraryManagement_MavenSetup/src/main/java/com/info/initializer/DemoInitializer.java
@@ -0,0 +1,7 @@
+package com.info.initializer;
+
+public class DemoInitializer {
+    public static void main(String[] args) {
+        System.out.println("Library Management Maven Setup Initialized Succesfully");
+    }
+}
