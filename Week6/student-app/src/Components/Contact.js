5c4911c70b7324b14df26d2139cda7c7ec962669
siddumr <mrsiddu2112@gmail.com>
2025-07-27 23:01:37 +0530

    task2

---
+++ Week6/student-app/src/Components/Contact.js
@@ -0,0 +1,12 @@
+// Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Welcome to the Contact page of the Student Management Portal</h2>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ Week6/student-app/src/Components/Home.js
@@ -0,0 +1,12 @@
+// Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to the Home page of Student Management Portal</h2>
+    </div>
+  );
+};
+
+export default Home;
